9d82743442e984f9fef4d7d2776730577128e754
Liluter <karolawdziewicz@gmail.com>
2023-05-07 18:47:27 +0200

    First commit after local work

---
+++ index.js
@@ -0,0 +1,87 @@
+let Xposition;
+let Yposition;
+document.addEventListener('mousemove', (e)=> {
+  Xposition= e.clientX;
+  Yposition= e.clientY;
+})
+
+let windowWidth = window.innerWidth;
+let windowHeight = windowWidth * 0.70836591; // preserve aspect ratio of textures
+
+// Create the application helper and add its render target to the page
+//
+let app = new PIXI.Application({width: windowWidth, height: windowHeight - 4});
+globalThis.__PIXI_APP__ = app;
+// document.body.appendChild(app.view);
+document.getElementById('canvas').appendChild(app.view);
+app.resizeTo = document.getElementById('canvas');
+
+// Asset loading from spritesheet
+const sheet = await PIXI.Assets.load("assets/Scaledspritesheetdata.json");
+
+// Create background Sprite from sheet loaded line above
+let background = new PIXI.Sprite(sheet.textures["aircraft-kindle-000"]);
+background.width = windowWidth; // seting actual window size 
+background.height = windowHeight; // seting actual window size 
+
+//Create Xray overlay Sprite
+let xRay = new PIXI.Sprite(sheet.textures["aircraft-kindle-Xray"]);
+xRay.width = windowWidth; // seting actual window size 
+xRay.height = windowHeight; // seting actual window size 
+
+//Create Scope  Sprite
+let scope = new PIXI.Sprite(sheet.textures["ClassicScope"]);
+
+//Add background area to show
+app.stage.addChild(background);
+
+// Create a mask object to define our mask
+let mask = new PIXI.Graphics();
+// Add the mask geometry and empty fill
+mask.beginFill();
+mask.drawCircle(125,125,125);
+mask.endFill();
+
+// Add container that will hold our masked content
+let maskContainer = new PIXI.Container();
+
+// Add container to show
+app.stage.addChild(maskContainer);
+// Set the mask to use our graphics object from above
+maskContainer.mask = mask;
+// Add the mask as a child, so that the mask is positioned relative to its parent
+maskContainer.addChild(mask);
+
+// Add the Xray layer as a child, so that is positioned relative to its parent
+maskContainer.addChild(xRay);
+
+// Add scope layer here to show above mask
+app.stage.addChild(scope);
+
+// Che
+window.addEventListener('resize', () => {
+  windowWidth = window.innerWidth;
+  windowHeight = windowWidth * 0.70836591;
+  
+  app.view.width = windowWidth;
+  app.view.height = windowHeight - 4 ;
+  
+  background.width = windowWidth;
+  background.height = windowHeight;
+  xRay.width = windowWidth;
+  xRay.height = windowHeight;
+  maskContainer.width = windowWidth;
+  maskContainer.height = windowHeight;
+  
+  app.resize()
+})
+
+// Add a ticker callback to scroll the text up and down
+let elapsed = 0.0;
+app.ticker.add((delta) => {
+  elapsed += delta;
+  
+  mask.position.set(Xposition - 100,Yposition - 100 )
+  scope.position.set(Xposition - 100,Yposition - 100 )
+});
+
---
+++ readme.md
@@ -0,0 +1,29 @@
+## Exercise with Aeroplane Image and Kindle Icon
+
+### Idea
+- After spotting the kindle icon on the desktop and the desktop background. I thought it was possible to combine the two images in an interactive way.
+---
+### Reasons:
+- Main reason to do this project was to check Library Pixi.js   and how it works.
+- Reson why try Pixi.js born after using .lottie files without using bodymovien plugin for Adobe After Effects. ( free trial only 7 days)
+- Created lottie .json files supposed to be small but came out big.
+---
+###  Workflow
+- Prepairing XRay layer texture in Gimp.
+- Crate Scope texture in Inkskape.
+- Use TexturePacker for preparation spritesheet file in .json very similar to .lottie files
+- coding main code.
+- add responsivnes.
+
+### Conclusions
+  - Pixi is easy in handle and it was just first steps with this library.
+  - Next step would be create ready to implement scrolling animation from bunch of .jpg frames
+---
+### Features
+- make some responsivnes with canvas.
+---
+### Tools
+- Inkscape https://inkscape.org
+- Gimp https://www.gimp.org
+- Pixi.js https://pixijs.com repo (https://github.com/pixijs/pixijs)
+- TexturePacker https://www.codeandweb.com/texturepacker (7 days pro trial)
